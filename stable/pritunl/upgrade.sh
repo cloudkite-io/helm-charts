078b9092bd3f8818e689d4ba38de4c5e03afdd20
Rodolfo Rodriguez <rodolfo@cloudkite.io>
2019-10-24 08:38:54 -0500

    Prompts user for confirmation in upgrade script

--- stable/pritunl/upgrade.sh
+++ stable/pritunl/upgrade.sh
@@ -1,9 +1,23 @@
 version=$1
 
+# Exit after any error
+set -e
+
 if [[ "$version" == "" ]]; then
         echo "Usage ./upgrade.sh <new-version>"
         exit
 fi
+
+helm template .
+echo "Do you want to upgrade to this new chart?
+Only 'yes' will be accepted to approve."
+read -p "Enter a value: " shouldContinue
+ 
+ if [[ "$shouldContinue" != "yes" ]]; then
+         echo "You have canceled the upgrade"
+         exit
+ fi
+
 
 cat Chart.yaml | sed "s/version.*/version: $version/g" > Chart.temp.yaml
 mv Chart.temp.yaml Chart.yaml
